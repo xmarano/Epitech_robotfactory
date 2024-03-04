72c7b70a2b7e38e15dbdb81bd70ccfb8dd3a84e3
xmarano <leo.gregori@epitech.eu>
2024-03-04 23:32:27 +0100

    ADD : error handle (name)

--- main.c
+++ main.c
@@ -7,6 +7,40 @@
 #include "rf.h"
 #include "my.h"
 
+static int check_header_name(int argc, char **argv, char *str, int i)
+{
+    char *name = ".name ";
+
+    for (int j = 0; j < 6; j++) {
+        if (name[j] != str[i + j])
+            return 84;
+    }
+    return 0;
+}
+
+int check_header(int argc, char **argv)
+{
+    FILE *file;
+    char *str = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *comment = ".comment ";
+    int verif = 0;
+
+    file = fopen(argv[1], "r");
+    read = getline(&str, &len, file);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' || verif == 1)
+            continue;
+        if (check_header_name(argc, argv, str, i) == 84)
+            return 84;
+        verif = 1;
+    }
+    free(str);
+    fclose(file);
+    return 0;
+}
+
 int error(int argc, char **argv)
 {
     FILE *file;
@@ -42,9 +76,11 @@ int tiret_h(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
+    S_t s;
+
     if (tiret_h(argc, argv) == 1)
         return 0;
-    if (error(argc, argv) == 84)
+    if (error(argc, argv) == 84 || check_header(argc, argv) == 84)
         return 84;
     return 0;
 }
